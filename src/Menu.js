853e432e0e81cce5c942b1e443d848d79fdfea1c
joshuscurtis <joshuscurtis@gmail.com>
2020-10-02 21:10:39 +0100

    loading fix

--- src/Menu.js
+++ src/Menu.js
@@ -7,25 +7,26 @@ import Product from "./Product";
 export default function ItemBuilder(props) {
   const menu = props.menu;
   const type = props.type;
-
-  var rows = [];
-  for (var i = 0; i < menu.length; i++) {
-    if (type === menu[i].type && menu[i].forSale === true) {
-      rows.push(
-        <Product
-          key={menu[i].name}
-          name={menu[i].name}
-          price={menu[i].price}
-          desc={menu[i].desc}
-          image={menu[i].image}
-          allegens={menu[i].allegens}
-        />
-      );
-    }
-  }
   if (props.menu === "loading") {
     return <h1>Loading</h1>;
-  } else {
+  }
+  if (props.menu !== "loading") {
+    var rows = [];
+    for (var i = 0; i < menu.length; i++) {
+      if (type === menu[i].type && menu[i].forSale === true) {
+        rows.push(
+          <Product
+            key={menu[i].name}
+            name={menu[i].name}
+            price={menu[i].price}
+            desc={menu[i].desc}
+            image={menu[i].image}
+            allegens={menu[i].allegens}
+          />
+        );
+      }
+    }
+
     return (
       <div className="Menu__Container">
         <Typography className="Menu__Title" variant="h5" component="h5">
